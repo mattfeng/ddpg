6ca246d9433b0dac9fa5bd531493bdd3e8b919b2
mattfeng <mattfeng@mit.edu>
2020-05-02 14:20:32 -0400

    ddpg works

--- scripts/ddpg.py
+++ scripts/ddpg.py
@@ -31,6 +31,8 @@ class ValueModel(nn.Module):
         action = self.process_action(action_in)  # (? x a_dim)
         sa = torch.cat([state, action], dim=1)   # (? x (s_dim + a_dim))
 
+        # print("sa tensor", sa)
+
         value = torch.relu(self.f1(sa))
         value = self.f2(value)
 
@@ -43,9 +45,8 @@ class PolicyModel(nn.Module):
 
         self.state_dim = state_dim
         self.action_dim = action_dim
-        self.f1 = nn.Linear(self.state_dim, 128)
-        self.f2 = nn.Linear(128, 128)
-        self.f3 = nn.Linear(128, self.action_dim)
+        self.f1 = nn.Linear(self.state_dim, 32)
+        self.f2 = nn.Linear(32, self.action_dim)
         
     def process_state(self, state):
         return state
@@ -53,8 +54,7 @@ class PolicyModel(nn.Module):
     def forward(self, state_in):
         state = self.process_state(state_in)
         action = torch.relu(self.f1(state))
-        action = torch.relu(self.f2(action))
-        action = self.f3(action)
+        action = self.f2(action)
 
         return action
 
@@ -67,14 +67,18 @@ def bellman_loss(
     """
     sarsd_arr: Array of 5 arrays s, a, r, sp, d.
     """
-    s, a, r, sp, d = [torch.from_numpy(np.array(x, dtype=np.float32)) for x in sarsd_arr]
-    tgt_actions = tgt_pol_net(sp)
-    # a better prediction of Q(s, a) = E[r + gamma * max[a']Q(s', a')]
-    tgt_val = r + discount * (1 - d) * tgt_val_net(sp, tgt_actions)
+    s, a, r, sp, d = [torch.as_tensor(x).float() for x in sarsd_arr]
+
     cur_val = opt_val_net(s, a)
 
-    return torch.mean((tgt_val - cur_val) ** 2)
+    with torch.no_grad():
+        tgt_actions = tgt_pol_net(sp)
+        # a better prediction of Q(s, a) = E[r + gamma * max[a']Q(s', a')]
+        tgt_val = r + discount * (1 - d) * tgt_val_net(sp, tgt_actions)
+        # print("tgt_val", tgt_val)
+
+    return ((tgt_val - cur_val) ** 2).mean()
 
 def policy_loss(sarsd_arr, opt_val_net, opt_pol_net):
-    s, _, _, _, _ = [torch.from_numpy(np.array(x, dtype=np.float32)) for x in sarsd_arr]
-    return -torch.mean(opt_val_net(s, opt_pol_net(s)))
+    s, _, _, _, _ = [torch.as_tensor(x).float() for x in sarsd_arr]
+    return -opt_val_net(s, opt_pol_net(s)).mean()
